dcb99fc13b38c807624e5d50bcfd10a3b0d91846
bemin <bemindfull21@gmail.com>
2026-09-09 20:45:07 +0900

    sql/011: 오너(호랭이/bemindfull21) 텔레그램 이력 6행 이관 완료

--- sql/011_migrate_owner_weights.sql
+++ sql/011_migrate_owner_weights.sql
@@ -1,9 +1,9 @@
 -- 오너의 텔레그램 몸무게 이력(admin.weight_log, tg_user_id='8974917114')을
--- 새 weight_entry 로 복사. 오너가 새 아이디로 가입한 뒤 <LOGIN_ID> 를 바꿔 실행.
--- healthweb 유저로 실행 (admin.weight_log 에 select 권한 있음).
+-- 새 weight_entry 로 복사. healthweb 유저로 실행 (admin.weight_log 에 select 권한 있음).
+-- ✅ 2026-09-09 실행됨: login_id='bemindfull21' (호랭이), 6행 이관.
 
 insert into healthweb.weight_entry (login_id, logged_at, weight, note)
-select '<LOGIN_ID>', log_date, weight, quote
+select 'bemindfull21', log_date, weight, quote
 from admin.weight_log
 where tg_user_id = '8974917114'
   and weight is not null;
